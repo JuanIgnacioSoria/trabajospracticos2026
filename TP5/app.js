cd95177a631a2ecf7da68c849f61496b0c81762e
Nacho <52178068@ivt.edu.ar>
2026-06-02 09:56:42 -0300

    Javascript

---
+++ TP5/app.js
@@ -0,0 +1,9 @@
+//Declarar variable
+//| | | | | | | | | | | | | | | |'Juan Fer Quintero' | | | | | | foto.jpg| | | | | | |
+//Asignar valor
+let numero1 = 10
+//Declarar y asignar al mismo tiempo
+let numero2 = 30
+numero1 = 5
+//Mostrar información
+console
