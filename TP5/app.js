8e4714b6f9572b8d3ad005d6d3363b2f2976fb95
Guillee-B <52178068@ivt.edu.ar>
2026-06-04 10:46:28 -0300

    4/6

--- TP5/app.js
+++ TP5/app.js
@@ -1,4 +1,13 @@
 //Esto es una suma profe
-let numero1 = 2
-let numero2 = 4
+let numero1 = 4
+let numero2 = 2
 let suma = numero1 + numero2
+
+//Esta es una resta profe
+let resta = numero1 - numero2
+
+//Esta es una multiplicacion profe
+let multiplicacion = numero1 * numero2
+
+//Esta es una division profe
+let division = numero1 / numero2
