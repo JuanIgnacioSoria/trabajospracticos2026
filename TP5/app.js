d85e83443cf44feff1d8263dbdcd9f6162c0f513
Francesca Artola <52178068@ivt.edu.ar>
2026-06-11 10:30:04 -0300

    Trabajo Practico

--- TP5/app.js
+++ TP5/app.js
@@ -1,13 +1,66 @@
-//Esto es una suma profe
-let numero1 = 4
-let numero2 = 2
-let suma = numero1 + numero2
+let numero1 = 2;
+let numero2 = 4;
 
-//Esta es una resta profe
-let resta = numero1 - numero2
+//Esto es una suma
+let suma = numero1 + numero2;
+//Esto es una resta
+let resta = numero1 - numero2;
+//Esto es una multiplicacion
+let multiplicacion = numero1 * numero2;
+//Esto es una division
+let division = numero1 / numero2;
 
-//Esta es una multiplicacion profe
-let multiplicacion = numero1 * numero2
+//Esto es una alerta del resultado de las operaciones
+alert("El resultado de la suma es: " + suma);
+alert("El resultado de la resta es: " + resta);
 
-//Esta es una division profe
-let division = numero1 / numero2
+//Aca se muestra el resultado de la division y multiplicacion  en la consola
+console.log("El resultado de la multiplicación es: " + multiplicacion);
+console.log("El resultado de la división es: " + division);
+
+//Aca estan los datos del alumno
+let nombreAlumno = "Juan Ignacio";
+let apellidoAlumno = "Soria";
+let edadAlumno = 14;
+
+console.log(
+    "El alumno: " +
+    nombreAlumno +
+    " " +
+    apellidoAlumno +
+    " tiene " +
+    edadAlumno +
+    " años"
+);
+
+//Aca se suman los precios
+let precio1 = 30;
+let precio2 = 100;
+let precio3 = 50;
+
+let total = precio1 + precio2 + precio3;
+
+console.log("El total a pagar es: $" + total);
+
+//Aca esta la frase del alumno
+let comidaFavorita = "Fideos con tuco";
+let colorFavorito = "Rojo";
+let numeroFavorito = 8;
+
+console.log(
+    "Mi comida favorita es " +
+    comidaFavorita +
+    ", me gusta el color " +
+    colorFavorito +
+    " y mi numero favorito es " +
+    numeroFavorito
+);
+
+//Aca cambio los valores
+let pais = "Argentina";
+
+console.log("Pais: " + pais);
+
+pais = "Brasil";
+
+console.log("Pais: " + pais);
