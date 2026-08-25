f4c34eafb7f95606adddcd70523280f3e062b562
Nacho <52178068@ivt.edu.ar>
2026-08-25 10:00:34 -0300

    si

--- TP7/java.js
+++ TP7/java.js
@@ -1,13 +1,34 @@
-let Boton = document.querySelector('Edad')
+let Boton = document.querySelector('#Edad')
 let edad = 15
-let Parrafo
+let Parrafo = document.querySelector('#Parrafo')
+let NombreUsuario = 'Nahuel'
+let nombreUsuario = 'Marcos'
+let Boton2 = document.querySelector('#Nombre')
+
 
 
 Boton.onclick = function () {
     if (edad >= 18) {
-        Parrafo.textcontent = 'ingreso aceptado'
+        Parrafo.textContent = 'ingreso aceptado'
     } else {
         Parrafo.textContent = 'ingreso rechazado'
     }
 }
 
+Boton2.onclick = function (){
+    if (NombreUsuario == 'Nahuel') {
+        Parrafo.textContent = "Bienvenido Nahuel, ¿cómo estás?"
+    } else {
+        Parrafo.textContent = "Bienvenido usuario"
+        
+    }
+}
+
+Boton2.onclick = function (){
+    if (NombreUsuario == 'Marcos') {
+        Parrafo.textContent = "Bienvenido Marcos, ¿cómo estás?"
+    } else {
+        Parrafo.textContent = "Bienvenido" 
+        
+    }
+}
